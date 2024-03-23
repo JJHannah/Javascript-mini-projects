2a35dd713244d27d4d206af8544a964e875c862b
JJHannah <zemjefferyzem@gmail.com>
2024-03-23 19:01:25 +0300

    add js selectors for buttons

--- JS-calculator/src/utils/calculator.js
+++ JS-calculator/src/utils/calculator.js
@@ -1,6 +1,12 @@
 const calculator = () => {
   const btn = document.querySelectorAll(".btnClick");
   const input = document.querySelector(".input");
+  const multiply = document.querySelector(".btnMultiply");
+  const subtract = document.querySelector("btnSubtract");
+  const add = document.querySelector(".btnAdd");
+  const divide = document.querySelector(".btnDivide");
+  const calculate = document.querySelector(".btnClculate");
+  const clear = document.querySelector(".btnClear");
 
   btn.addEventListener("click", appendToDisplay);
 
