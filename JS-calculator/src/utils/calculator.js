d0797fe32e3c62c606ae531cd3120dacc24cc940
JJHannah <zemjefferyzem@gmail.com>
2024-03-23 18:42:53 +0300

    fix bug on  button click event

---
+++ JS-calculator/src/utils/calculator.js
@@ -0,0 +1,12 @@
+const calculator = () => {
+  const btn = document.querySelectorAll(".btnClick");
+  const input = document.querySelector(".input");
+
+  btn.addEventListener("click", appendToDisplay);
+
+  function appendToDisplay() {
+    let calculation = "";
+  }
+};
+
+export default calculator;
