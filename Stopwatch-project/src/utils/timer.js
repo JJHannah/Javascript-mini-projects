723c382e5b839cc7f32b8f96cea410ace3a7b123
JJHannah <zemjefferyzem@gmail.com>
2024-03-30 17:12:55 +0300

    fix eventListeners on buttons

--- Stopwatch-project/src/utils/timer.js
+++ Stopwatch-project/src/utils/timer.js
@@ -4,13 +4,30 @@ const timer = () => {
   const btnStop = document.querySelector(".btnStop");
   const btnReset = document.querySelector(".btnReset");
 
-  btnStart.addEventListener("click", start);
-  btnStop.addEventListener("click", stop);
-  btnReset.addEventListener("click", reset);
+  btnStart.addEventListener("click", startWatch);
+  btnStop.addEventListener("click", stopWatch);
+  btnReset.addEventListener("click", resetWatch);
 
-  function start() {}
-  function stop() {}
-  function reset() {}
+  let startTime;
+  let stopWatchInterval;
+  let elapsedPausedTime = 0;
+
+  function startWatch() {
+    if (!stopWatchInterval) {
+      startTime = new Date().getTime() - elapsedTime;
+      stopWatchInterval = setInterval(updateStopwatch, 1000);
+    }
+  }
+  function stopWatch() {
+    clearInterval(stopWatchInterval);
+    elapsedPausedTime = new Date().getTime() - startTime;
+    stopWatchInterval = null;
+  }
+  function resetWatch() {
+    stopWatch();
+    elapsedPausedTime = 0;
+    displayNum.innerHTML = "00:00:00";
+  }
 };
 
 export default timer;
