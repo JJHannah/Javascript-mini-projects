3a98dbf46bb3fa5c1c8c39a4e9bcdb081e8771c9
JJHannah <zemjefferyzem@gmail.com>
2024-04-02 18:28:55 +0300

    fix index and styles format

--- Modal-window/src/utils/app.js
+++ Modal-window/src/utils/app.js
@@ -1,6 +1,7 @@
 const app = () => {
   const showModal = document.querySelectorAll(".show-modal");
   const closeModal = document.querySelector(".close-modal");
+  const modal = document.getElementById("modal")
 };
 
 export default app;
