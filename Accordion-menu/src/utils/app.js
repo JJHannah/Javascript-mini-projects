e3e5b910e008123e1313a2a331b27b376f1c0690
JJHannah <zemjefferyzem@gmail.com>
2024-04-23 18:04:12 +0300

    Add remove classlist

--- Accordion-menu/src/utils/app.js
+++ Accordion-menu/src/utils/app.js
@@ -1,3 +1,10 @@
-const menu = () => {};
+const menu = () => {
+  const content = document.querySelectorAll(".accordion__content");
+
+  content.forEach((value) => {
+    console.log(value);
+    content.classList.remove("active");
+  });
+};
 
 export default menu;
