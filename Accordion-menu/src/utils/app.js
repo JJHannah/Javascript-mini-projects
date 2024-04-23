65054216e0e1a3f6064a2b9c028fb0535e2af9ec
JJHannah <zemjefferyzem@gmail.com>
2024-04-23 17:50:15 +0300

    fix classlist toggle

--- Accordion-menu/src/utils/app.js
+++ Accordion-menu/src/utils/app.js
@@ -1,3 +1,9 @@
-const menu = () => {};
+const menu = () => {
+  const content = document.querySelectorAll(".accordion__content");
+  for (i = 0; i < content.length; i++) {
+    content[i].addEventListener("click", select);
+    this.classList.remove("active");
+  }
+};
 
 export default menu;
