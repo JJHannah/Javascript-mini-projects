bce9b5e0c915c68068ded605a07d6e6b22084389
JJHannah <zemjefferyzem@gmail.com>
2024-04-30 18:41:11 +0300

    fix event classlist toggle

--- Accordion-menu/src/utils/app.js
+++ Accordion-menu/src/utils/app.js
@@ -1,9 +1,12 @@
 const menu = () => {
   const content = document.querySelectorAll(".accordion__content");
 
-  content.forEach((value, index) => {
-    let description = value.querySelector(".description");
-    console.log(description);
+  content.forEach((text) => {
+    text.addEventListener("click", () => {
+      let description = text.querySelector(".description");
+      console.log(description);
+      text.classList.toggle("open");
+    });
   });
 };
 
