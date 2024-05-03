2ff1b3ac1069c95f72618bb95dd16980be80a5b8
JJHannah <zemjefferyzem@gmail.com>
2024-05-03 14:03:31 +0300

    fix accordion css display

--- Accordion-menu/src/utils/app.js
+++ Accordion-menu/src/utils/app.js
@@ -1,16 +1,31 @@
 const menu = () => {
   const subtitles = document.querySelectorAll(".subtitle");
+  const openSubtitles = document.querySelectorAll(".is-open");
+
+  // subtitles.forEach((subtitle) => {
+  //   subtitle.addEventListener("click", () => {
+  //     if (subtitle.classList.contains("is-open")) {
+  //       subtitle.classList.remove("is-open");
+  //     } else {
+  //       const openSubtitles = document.querySelectorAll(".is-open");
+  //       openSubtitles.forEach((openSubtitles) => {
+  //         openSubtitles.classList.remove("is-open");
+  //       });
+  //       openSubtitles.classList.add("is-open");
+  //     }
+  //   });
+  // });
 
   subtitles.forEach((subtitle) => {
     subtitle.addEventListener("click", () => {
       if (subtitle.classList.contains("is-open")) {
         subtitle.classList.remove("is-open");
       } else {
-        const openSubtitles = document.querySelectorAll(".is-open");
         openSubtitles.forEach((openSubtitles) => {
-          openSubtitles.classList.remove("is-open");
+          if (openSubtitles.classList.remove("is-open")) {
+            openSubtitles.classList.add("is-open");
+          }
         });
-        openSubtitles.classList.add("is-open");
       }
     });
   });
