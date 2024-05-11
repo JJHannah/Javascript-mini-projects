f11bb9cbb8f586562b4c3f60ef6167fabee13b95
JJHannah <zemjefferyzem@gmail.com>
2024-05-11 19:18:37 +0300

    fix api url link

--- Weather-App/src/utils/app.js
+++ Weather-App/src/utils/app.js
@@ -1,2 +1,9 @@
-const weatherApp = () => {};
+const weatherApp = () => {
+  const apiKey = "YOUR_API_KEY";
+  // const apiUrl;
+  const location = document.querySelector(".location");
+  const temperature = document.querySelector(".temperature");
+  const description = document.querySelector(".description");
+  const btnSearch = document.querySelector(".btnSearch");
+};
 export default weatherApp;
