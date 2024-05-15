a1899696c3f230849326b5f8af75b0124f58afe3
JJHannah <zemjefferyzem@gmail.com>
2024-05-15 20:58:17 +0300

    fix broken api url link

--- Weather-App/src/utils/app.js
+++ Weather-App/src/utils/app.js
@@ -6,12 +6,19 @@ const weatherApp = () => {
   const descriptionElem = document.querySelector(".description");
   const inputElem = document.querySelector(".inputElem");
   const btnSearch = document.querySelector(".btnSearch");
-  const apikey = "";
-  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputElem}&appid=${apiKey}&units=metric`;
+  // const apikey = "";
+  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputElem}&appid=${apiKey}&units=metric`;
 
-  btnSearch.addEventListener("click", () => {
-    const weatherSearch = inputElem.value;
-    console.log(weatherSearch);
-  });
+  btnSearch.addEventListener("click", fetchData);
+
+  async function fetchData() {
+    try {
+      const weatherSearch = inputElem.value;
+      console.log(weatherSearch);
+    } catch (error) {
+      console.log(error);
+    }
+  }
 };
+
 export default weatherApp;
