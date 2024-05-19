6ef4231a960f388a974bbfb43e7746ea2e1e2f8a
JJHannah <zemjefferyzem@gmail.com>
2024-05-19 14:53:03 +0300

    Add api key url

--- Weather-App/src/utils/app.js
+++ Weather-App/src/utils/app.js
@@ -1,13 +1,11 @@
 const weatherApp = () => {
-  // const apiKey = "YOUR_API_KEY";
-  // const apiUrl;
   const locationElem = document.querySelector(".location");
   const temperatureElem = document.querySelector(".temperature");
   const descriptionElem = document.querySelector(".description");
   const inputElem = document.querySelector(".inputElem");
   const btnSearch = document.querySelector(".btnSearch");
   // // const apiKey = "";
-  // const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputElem}&appid=${apiKey}&units=metric`;
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${inputElem}&appid=${apiKey}&units=metric`;
 
   btnSearch.addEventListener("click", fetchData);
 
