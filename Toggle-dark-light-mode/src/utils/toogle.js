21c4c74ecaf787382e8f50849330a838f69e8459
Hannah <112068752+JJHannah@users.noreply.github.com>
2024-06-19 17:03:04 +0300

    Update toogle.js

--- Toggle-dark-light-mode/src/utils/toogle.js
+++ Toggle-dark-light-mode/src/utils/toogle.js
@@ -5,7 +5,7 @@ const toggle = () => {
   toggle.addEventListener("click", () => {
     element.classList.toggle("dark-mode");
   });
-  localStorage.setItem();
+  localStorage.setItem(dark-mode);
 };
 
 export default toggle;
