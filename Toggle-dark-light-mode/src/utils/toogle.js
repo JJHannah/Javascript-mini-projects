e97d91a8968139b8b576f1fecc825b1d02bd68d1
JJHannah <zemjefferyzem@gmail.com>
2024-08-18 14:58:01 +0300

    Fix toogle link file to main.js

---
+++ Toggle-dark-light-mode/src/utils/toogle.js
@@ -0,0 +1,26 @@
+const toggle = () => {
+  const toggleBtn = document.querySelector(".toggleBtn");
+
+  const toggleTheme = () => {
+    if (document.body.classList.contains("dark-mode")) {
+      localStorage.setItem("theme", "light");
+      document.body.classList.remove("dark-mode");
+    } else {
+      localStorage.setItem("theme", "dark");
+      document.body.classList.add("dark-mode");
+    }
+  };
+
+  const enabledTheme = () => {
+    const appliedTheme = localStorage.getItem("theme");
+    if (appliedTheme === "dark") {
+      document.body.classList.add("dark-mode");
+    } else {
+      document.body.classList.r("dark-mode");
+    }
+  };
+  toggleBtn.addEventListener("click", toggleTheme);
+  enabledTheme();
+};
+
+export default toggle;
