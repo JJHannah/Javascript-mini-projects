08ac48e20d91eacea4b6bc027552f366f181e577
JJHannah <zemjefferyzem@gmail.com>
2024-11-20 07:23:39 +0300

    Fix color event target

--- Color-Picker/src/Components/ColorPicker.jsx
+++ Color-Picker/src/Components/ColorPicker.jsx
@@ -1,1 +1,24 @@
-export default function ColorPicker() {}
+import { useState } from "react";
+
+export default function ColorPicker() {
+  const [color, setColor] = useState("#FFFFFF");
+
+  function handleColorPicker(event) {
+    setColor(event.target.value);
+  }
+  return (
+    <div className="container">
+      <h1>Color Picker</h1>
+      <label htmlFor="color">Pick a Color</label>
+      <br />
+      <input
+        name="color"
+        type="color"
+        value={color}
+        id="bgColor"
+        onChange={handleColorPicker}
+      />
+      <p>Selected color: {color}</p>
+    </div>
+  );
+}
