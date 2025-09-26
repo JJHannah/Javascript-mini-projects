939ee9e71c4614ee380cb394ae6f63a2baffda02
JJHannah <zemjefferyzem@gmail.com>
2025-09-26 23:58:05 +0300

    Create .env separate  folders

---
+++ Dictionary-app/src/components/Mycomponent.jsx
@@ -0,0 +1,9 @@
+function MyDictionary() {
+  return (
+    <>
+      <input type="text" placeholder="type any word" value="" />
+      <button></button>
+    </>
+  );
+}
+export default MyDictionary;
