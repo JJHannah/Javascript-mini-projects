891dded19efa84356353ea1c90412e223cf47bfc
JJHannah <zemjefferyzem@gmail.com>
2025-09-30 21:36:33 +0300

    Fix the iteration on array images

---
+++ Image-slider/src/components/MyPictures.jsx
@@ -0,0 +1,14 @@
+function PictureSlide() {
+  const imageArray = [
+    " https://images.unsplash.com/photo-1758671451540-58f5ef5a49ea?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDZ8dG93SlpGc2twR2d8fGVufDB8fHx8fA%3D%3D",
+    " https://images.unsplash.com/photo-1758575603807-1f01152edcb5?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDI3fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+    "https://images.unsplash.com/photo-1758221943117-6e1f900b957a?w=400&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHx0b3BpYy1mZWVkfDU4fHRvd0paRnNrcEdnfHxlbnwwfHx8fHw%3D",
+  ];
+
+  return (
+    <div className="container">
+      <p></p>
+    </div>
+  );
+}
+export default PictureSlide;
