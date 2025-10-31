66c87334f45eaddd163a684f78c61994d0d92b63
JJHannah <zemjefferyzem@gmail.com>
2025-10-31 18:43:08 +0300

    Remove jsx file, work on app file

--- Image-slider/src/utils/app.js
+++ Image-slider/src/utils/app.js
@@ -1,1 +1,16 @@
-const images = [];
+const images = [
+  "https://images.unsplash.com/photo-1654110455429-cf322b40a906?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=400",
+  "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=400",
+];
+btnStart.addEventListener("click", movePic);
+btnEnd.addEventListener("click", stopAnimation);
+
+function movePic() {
+  pictures.forEach((pic, i) => {
+    setTimeout(() => {
+      const image = document.createElement("img");
+      image.src = pic;
+      container.append(image);
+    }, i * 2000);
+  });
+}
