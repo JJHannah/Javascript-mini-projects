52478c52f9103a9cbd9ac13872e56fec7e0c370e
JJHannah <zemjefferyzem@gmail.com>
2025-11-02 16:36:38 +0300

    Add the fetch async to the pics taking long to load

--- Image-slider/src/utils/app.js
+++ Image-slider/src/utils/app.js
@@ -1,12 +1,16 @@
+const btnStart = document.querySelector(".btnStart");
+const btnEnd = document.querySelector(".btnStop");
+const container = document.querySelector(".container");
+
 const images = [
   "https://images.unsplash.com/photo-1654110455429-cf322b40a906?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NHx8cHJvZmlsZSUyMHBpY3R1cmV8ZW58MHx8MHx8fDA%3D&auto=format&fit=crop&q=60&w=400",
   "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTF8fHByb2ZpbGUlMjBwaWN0dXJlfGVufDB8fDB8fHww&auto=format&fit=crop&q=60&w=400",
 ];
 btnStart.addEventListener("click", movePic);
-btnEnd.addEventListener("click", stopAnimation);
+// btnEnd.addEventListener("click", stopAnimation);
 
 function movePic() {
-  pictures.forEach((pic, i) => {
+  images.forEach((pic, i) => {
     setTimeout(() => {
       const image = document.createElement("img");
       image.src = pic;
@@ -14,6 +18,6 @@ function movePic() {
     }, i * 2000);
   });
 }
-_modules;
 
-function stopAnimation() {}
+// function stopAnimation() {}
+``;
