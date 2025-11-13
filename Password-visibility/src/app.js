fc13cc99e8e55a4248c4a2197ab9dfb22cecc8ec
JJHannah <zemjefferyzem@gmail.com>
2025-11-13 18:17:15 +0300

    Add toggle feature to the eye icon for visiblity

--- Password-visibility/src/app.js
+++ Password-visibility/src/app.js
@@ -1,14 +1,11 @@
-function password() {
-  const passwordInput = document.querySelector("#password");
-  const eyeClick = document.querySelector(".eye");
-  const eyeChange = document.getElementById("eyes");
+const passwordInput = document.querySelector("#password");
+const eyeClick = document.querySelector(".eye");
+const eyeChange = document.getElementById("eyes");
 
-  eyeClick.addEventListener("click", () => {
-    if (passwordInput.type === "password") {
-      passwordInput.setAttribute("type", "text");
-    } else {
-      passwordInput.setAttribute("type", "password");
-    }
-  });
-}
-export default password;
+eyeClick.addEventListener("click", () => {
+  if (passwordInput.type === "password") {
+    passwordInput.setAttribute("type", "text");
+  } else {
+    passwordInput.setAttribute("type", "password");
+  }
+});
